a871159175c9cdd6c64a818689904c9df11ec4f8
Rapheal555 <odejinmirapheal@gmail.com>
2023-01-13 22:07:04 +0100

    first commit

---
+++ 02_vars/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// var name = "Rapheal"
+	var age = 37
+	const isCool =true
+	name := "Brad"
+	size := 1.3
+
+	num, email := "10", "email@ddd.ff"
+
+	fmt.Println(name,age,isCool, num,email , size)
+	fmt.Printf("%T\n", name)
+}
---
+++ 03_packages/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/Rapheal555/go_crash_course/03_packages/strutil"
+	// "./strutil"
+)
+
+func main() {
+	fmt.Println(fmt.Println(math.Floor(33.456)))
+	fmt.Println(strutil.Reverse("olleh"))
+}
